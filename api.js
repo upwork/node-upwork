45ab9adc07f1a393d19a6ab7d2251f5afd2f818c
Maksym Novozhylov <mnovozhilov@upwork.com>
2015-05-06 08:40:13 +0200

    version 1.0.0

---
+++ api.js
@@ -0,0 +1,212 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Accounts",
+        "Applications",
+        "Auth",
+        "Billings",
+        "Client",
+        "Companies",
+        "Config",
+        "Debug",
+        "Earnings",
+        "Engagement",
+        "Engagements",
+        "Interviews",
+        "Jobs",
+        "Mc",
+        "Metadata",
+        "Offers",
+        "Payments",
+        "Profile",
+        "Roles",
+        "Search",
+        "Snapshot",
+        "Team",
+        "Teams",
+        "Time",
+        "UpworkApi",
+        "Users",
+        "Workdiary"
+    ],
+    "modules": [
+        "api",
+        "client",
+        "config",
+        "debug",
+        "routes.activities.engagement",
+        "routes.activities.team",
+        "routes.auth",
+        "routes.freelancers.profile",
+        "routes.freelancers.search",
+        "routes.hr.clients.applications",
+        "routes.hr.clients.offers",
+        "routes.hr.contractors.applications",
+        "routes.hr.contractors.offers",
+        "routes.hr.contracts",
+        "routes.hr.engagements",
+        "routes.hr.interviews",
+        "routes.hr.jobs",
+        "routes.hr.milestones",
+        "routes.hr.roles",
+        "routes.hr.submissions",
+        "routes.jobs.profile",
+        "routes.jobs.search",
+        "routes.mc",
+        "routes.metadata",
+        "routes.organization.companies",
+        "routes.organization.teams",
+        "routes.organization.users",
+        "routes.payments",
+        "routes.reports.finance.accounts",
+        "routes.reports.finance.billings",
+        "routes.reports.finance.earnings",
+        "routes.reports.time",
+        "routes.snapshot",
+        "routes.teams",
+        "routes.workdiary"
+    ],
+    "allModules": [
+        {
+            "displayName": "api",
+            "name": "api"
+        },
+        {
+            "displayName": "client",
+            "name": "client"
+        },
+        {
+            "displayName": "config",
+            "name": "config"
+        },
+        {
+            "displayName": "debug",
+            "name": "debug"
+        },
+        {
+            "displayName": "routes.activities.engagement",
+            "name": "routes.activities.engagement"
+        },
+        {
+            "displayName": "routes.activities.team",
+            "name": "routes.activities.team"
+        },
+        {
+            "displayName": "routes.auth",
+            "name": "routes.auth"
+        },
+        {
+            "displayName": "routes.freelancers.profile",
+            "name": "routes.freelancers.profile"
+        },
+        {
+            "displayName": "routes.freelancers.search",
+            "name": "routes.freelancers.search"
+        },
+        {
+            "displayName": "routes.hr.clients.applications",
+            "name": "routes.hr.clients.applications"
+        },
+        {
+            "displayName": "routes.hr.clients.offers",
+            "name": "routes.hr.clients.offers"
+        },
+        {
+            "displayName": "routes.hr.contractors.applications",
+            "name": "routes.hr.contractors.applications"
+        },
+        {
+            "displayName": "routes.hr.contractors.offers",
+            "name": "routes.hr.contractors.offers"
+        },
+        {
+            "displayName": "routes.hr.contracts",
+            "name": "routes.hr.contracts"
+        },
+        {
+            "displayName": "routes.hr.engagements",
+            "name": "routes.hr.engagements"
+        },
+        {
+            "displayName": "routes.hr.interviews",
+            "name": "routes.hr.interviews"
+        },
+        {
+            "displayName": "routes.hr.jobs",
+            "name": "routes.hr.jobs"
+        },
+        {
+            "displayName": "routes.hr.milestones",
+            "name": "routes.hr.milestones"
+        },
+        {
+            "displayName": "routes.hr.roles",
+            "name": "routes.hr.roles"
+        },
+        {
+            "displayName": "routes.hr.submissions",
+            "name": "routes.hr.submissions"
+        },
+        {
+            "displayName": "routes.jobs.profile",
+            "name": "routes.jobs.profile"
+        },
+        {
+            "displayName": "routes.jobs.search",
+            "name": "routes.jobs.search"
+        },
+        {
+            "displayName": "routes.mc",
+            "name": "routes.mc"
+        },
+        {
+            "displayName": "routes.metadata",
+            "name": "routes.metadata"
+        },
+        {
+            "displayName": "routes.organization.companies",
+            "name": "routes.organization.companies"
+        },
+        {
+            "displayName": "routes.organization.teams",
+            "name": "routes.organization.teams"
+        },
+        {
+            "displayName": "routes.organization.users",
+            "name": "routes.organization.users"
+        },
+        {
+            "displayName": "routes.payments",
+            "name": "routes.payments"
+        },
+        {
+            "displayName": "routes.reports.finance.accounts",
+            "name": "routes.reports.finance.accounts"
+        },
+        {
+            "displayName": "routes.reports.finance.billings",
+            "name": "routes.reports.finance.billings"
+        },
+        {
+            "displayName": "routes.reports.finance.earnings",
+            "name": "routes.reports.finance.earnings"
+        },
+        {
+            "displayName": "routes.reports.time",
+            "name": "routes.reports.time"
+        },
+        {
+            "displayName": "routes.snapshot",
+            "name": "routes.snapshot"
+        },
+        {
+            "displayName": "routes.teams",
+            "name": "routes.teams"
+        },
+        {
+            "displayName": "routes.workdiary",
+            "name": "routes.workdiary"
+        }
+    ]
+} };
+});
